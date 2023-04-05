8b5666f182a98edcc9deefc185b456a64ca133e1
Camilo Gonzalez <camilo.igv@gmail.com>
2023-04-05 08:58:27 -0400

    prueba

---
+++ downloadFile.py
@@ -0,0 +1,20 @@
+#get file from web url
+import urllib.request
+
+def downloadFile(url, path):
+    try:
+        urllib.request.urlretrieve(url, path)
+    except Exception as e:
+        print(e)
+        print("Error downloading file: " + url)
+        return False
+    return True
+
+def main():
+    url = "https://www.loteria.cl/loterianet/Content/sincronizacion/Kino.xls"
+    path = "C:\\Users\\Public\\Documents\\pythonFiles\\Kino.xls"
+    if downloadFile(url, path):
+        print("Downloaded file: " + path)
+
+if __name__ == "__main__":
+    main()  
