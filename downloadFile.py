61225d6e639b6f7c991ebc1963ae3035f80f1f4b
camilitwo-kraken <camilo.igv@gmail.com>
2023-04-12 18:57:07 -0400

    aaaa

--- downloadFile.py
+++ downloadFile.py
@@ -15,6 +15,31 @@ def main():
     path = "C:\\Users\\camil\\OneDrive\\Documentos\\pythonFiles\\Kino.xls"
     if downloadFile(url, path):
         print("Downloaded file: " + path)
+        # Path: uploadFile.py
+        if uploadFile(path, "Kino.xls"):
+            print("Uploaded file: " + "KN/KINO.xls")
+        else:
+            print("Error uploading file: " + path)
+
+
+
+#upload file to google drive
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+
+def uploadFile(path, name):
+    try:
+        gauth = GoogleAuth()
+        gauth.LocalWebserverAuth()
+        drive = GoogleDrive(gauth)
+        file = drive.CreateFile({'title': name})
+        file.SetContentFile(path)
+        file.Upload()
+        print("Uploaded file: " + path)
+    except Exception as e:
+        print(e)
+
+
 
 if __name__ == "__main__":
     main()  
